3f3090d7d85dcc0fbb9ade06f30e3c0cd07c8bd2
Sahithi03B <satyasahithi03.baddi@gmail.com>
2022-07-28 00:02:04 +0530

    eighth

--- sort.c
+++ sort.c
@@ -18,7 +18,7 @@ void insertionSort(int arr[], int n) {
 
         arr[j + 1] = key;
     }
-}
+}//sorting
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
