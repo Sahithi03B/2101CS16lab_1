ce84c5809d30419ce8b822990d4439f26a2349e8
Sahithi03B <satyasahithi03.baddi@gmail.com>
2022-07-27 23:38:02 +0530

    first

---
+++ cal_prog.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+   float i,j;
+   int n;
+   printf("Enter the first number: ");
+   scanf("%f",&i);
+   printf("Enter the second number: ");
+   scanf("%f",&j);
+   printf("Select 1 for addition\n2 for subtraction\n3 for multiplication\n4 for division\n");
+   printf("Which operation do you want to do: ");
+   scanf("%d",&n);
+   switch(n){
+    /*Addition*/
+    // hello world
+    case 1: printf("%.1f\n",i+j);
+    break;
+    return 0;
+   }
+}
