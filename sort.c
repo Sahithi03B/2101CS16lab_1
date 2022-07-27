d848b84b2b441e8e1334ebea3e926e28ca72a0b8
Sahithi03B <satyasahithi03.baddi@gmail.com>
2022-07-27 23:52:53 +0530

    fifth

---
+++ sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <stdbool.h>
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
